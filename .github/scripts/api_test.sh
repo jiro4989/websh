c055770613f2ea910041ba285c15222d2c7ee2b4
jiro4989 <jiroron666@gmail.com>
2020-02-20 00:35:31 +0900

    APIのテストスクリプトを修正 #145

--- .github/scripts/api_test.sh
+++ .github/scripts/api_test.sh
@@ -2,6 +2,15 @@
 
 set -eux
 
+docker-compose up -d
+sleep 5
+
+./bin/websh_remover &
+remover_pid=$!
+
+./tools/visor &
+visor_pid=$!
+
 pushd websh_server
 WEBSH_DOCKER_IMAGE=jiro4989/websh ./bin/websh_server &
 pid=$!
@@ -14,5 +23,9 @@ cnt=$(curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello", "images":[]}
 # 標準エラー出力のテスト
 cnt=$(curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello >&2", "images":[]}' 'http://0.0.0.0:5000/shellgei' | grep hello | wc -l)
 [ "$cnt" -eq 1 ]
+
 kill $pid
+kill $remover_pid
+kill $visor
+
 popd
