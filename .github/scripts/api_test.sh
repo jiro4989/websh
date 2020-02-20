19b79913b0fe08528c1f66cf21d6bc71a6bda999
jiro4989 <jiroron666@gmail.com>
2020-02-20 23:55:54 +0900

    CI用の設定を追加 #145

--- .github/scripts/api_test.sh
+++ .github/scripts/api_test.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-docker-compose up -d
+docker-compose -f docker-compose.yml -f docker-compose-ci-api.yml up -d
 sleep 5
 
 ./bin/websh_remover &
@@ -12,14 +12,17 @@ remover_pid=$!
 visor_pid=$!
 
 pushd websh_server
-WEBSH_DOCKER_IMAGE=jiro4989/websh ./bin/websh_server &
+./bin/websh_server &
 pid=$!
 sleep 2
+
 # 出力内容を確認する用
 curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello", "images":[]}' 'http://0.0.0.0:5000/shellgei'
+
 # 標準出力のテスト
 cnt=$(curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello", "images":[]}' 'http://0.0.0.0:5000/shellgei' | grep hello | wc -l)
 [ "$cnt" -eq 1 ]
+
 # 標準エラー出力のテスト
 cnt=$(curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello >&2", "images":[]}' 'http://0.0.0.0:5000/shellgei' | grep hello | wc -l)
 [ "$cnt" -eq 1 ]
