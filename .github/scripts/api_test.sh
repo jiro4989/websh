2720814a4887df26ce0dcc66429f63f872c244fe
jiro4989 <jiroron666@gmail.com>
2020-03-12 01:48:21 +0900

    waitを挟むように修正

--- .github/scripts/api_test.sh
+++ .github/scripts/api_test.sh
@@ -18,10 +18,12 @@ sleep 2
 
 # 出力内容を確認する用
 curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello", "images":[]}' 'http://0.0.0.0:5000/shellgei'
+sleep 3
 
 # 標準出力のテスト
 cnt=$(curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello", "images":[]}' 'http://0.0.0.0:5000/shellgei' | grep hello | wc -l)
 [ "$cnt" -eq 1 ]
+sleep 3
 
 # 標準エラー出力のテスト
 cnt=$(curl -s --connect-timeout 5 -X POST -d '{"code":"echo hello >&2", "images":[]}' 'http://0.0.0.0:5000/shellgei' | grep hello | wc -l)
