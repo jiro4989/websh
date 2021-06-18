0e63dc50d7e9f508f703683bcf095d139a3fa7d4
jiro4989 <jiroron666@gmail.com>
2021-06-18 23:42:25 +0900

    ある程度の操作とUIを追加

--- websh_front_v3/src/App.js
+++ websh_front_v3/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+
+function newUniqueId() {
+  const date = new Date().getTime().toString(16)
+  const rand = Math.floor(1000*Math.random()).toString(16)
+  return date + rand
+}
+
+function newHistory(shellgei) {
+  const id = newUniqueId()
+  const history = {
+    id: id,
+    shellgei: shellgei
+  }
+  return history
+}
 
 function App() {
+  const [shellgei, setShellgei] = useState("")
+  const [histories, setHistories] = useState([])
+
+  const runShellgei = (shell) => {
+    const history = newHistory(shell)
+    setHistories([...histories, history])
+    console.log(shell)
+    console.log(histories)
+  }
+
+  const historyElems = histories
+    .map((v) => <div key={v.id}>
+      <textarea className="shellgei-input" defaultValue={v.shellgei} />
+    </div>)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="terminal-area">
+        {historyElems}
+        <div>
+          <textarea
+            className="shellgei-input"
+            onChange={(e) => setShellgei(e.target.value)}
+            autoFocus
+          />
+        </div>
+        <div>
+          <button onClick={(e) => runShellgei(shellgei)}>Run</button>
+          <button>Tweet</button>
+        </div>
+      </div>
     </div>
   );
 }
