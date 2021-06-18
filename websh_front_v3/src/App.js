ca2515c2b27a6ec5b603c685bba5c60cb0739a21
jiro4989 <jiroron666@gmail.com>
2021-06-18 22:33:17 +0900

    Revert "とりあえずxterm.js"
    This reverts commit ab2bafb7ed507804f433ba7c8ec1fe7618273378.

--- websh_front_v3/src/App.js
+++ websh_front_v3/src/App.js
@@ -1,25 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { Terminal } from 'xterm';
-import {useEffect} from 'react';
 
 function App() {
-  const term = new Terminal()
-
-  useEffect(() => {
-    const termElem = document.getElementById("terminal")
-    term.open(termElem)
-    term.writeln("Welcome to websh.")
-    term.writeln("This is a web shellgei execution environment.")
-    term.writeln("Type your shellgei.")
-    term.writeln("")
-    term.writeln("")
-    term.writeln("$ echo sushi")
-    term.writeln("sushi")
-  })
-
   return (
     <div className="App">
-      <div id="terminal"></div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
