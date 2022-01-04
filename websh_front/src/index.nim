8ada0f0dac0e3a1609dc9349eca3de6f598d8e3c
jiro4989 <jiroron666@gmail.com>
2022-01-04 23:49:33 +0900

    fix: websh_frontのUIを変更

--- websh_front/src/index.nim
+++ websh_front/src/index.nim
@@ -22,6 +22,14 @@ type
   MediaObj = object
     name, data: cstring
 
+  ContentType = enum
+    ctInfo, ctHistory, ctStdout, ctStderr
+
+  Content = object
+    ## 画面に表示するコンテンツ
+    typ: ContentType
+    text: string
+
 const
   statusOk = cint(0)
   statusTimeout = cint(1)
@@ -59,6 +67,11 @@ var
   hashTag = cstring"シェル芸"
   shellHistory: seq[string]
 
+  contents = @[
+    Content(typ: ctInfo, text: "Welcome to websh!"),
+    Content(typ: ctInfo, text: "Run: `websh help`"),
+  ]
+
 # localstorageにシェルの履歴が存在するときだけ取得
 if localstorage.hasItem("history"):
   let hist = localstorage.getItem("history").`$`.parseJson.to(seq[string])
@@ -76,12 +89,14 @@ if 1 < query.len:
 
 proc respCb(httpStatus: int, response: cstring) =
   let resp = fromJson[ResponseResult](response)
-  outputStatus = resp.status
-  outputSystemMessage = resp.system_message
-  outputStdout = resp.stdout
-  outputStderr = resp.stderr
-  outputImages = resp.images
-  outputElapsedTime = resp.elapsed_time
+  contents.add(Content(typ: ctStdout, text: $resp.stdout))
+  contents.add(Content(typ: ctStderr, text: $resp.stderr))
+  # outputStatus = resp.status
+  # outputSystemMessage = resp.system_message
+  # outputStdout = resp.stdout
+  # outputStderr = resp.stderr
+  # outputImages = resp.images
+  # outputElapsedTime = resp.elapsed_time
   # シェルの実行中表示 OFF
   isProgress = false
 
@@ -89,6 +104,7 @@ proc sendShellButtonOnClick(ev: Event, n: VNode) = # シェルの実行中表示
   isProgress = true
   let images = inputImages.filterIt(it.name != cstring"").mapIt(it.data)
   let body = %*{"code": inputShell, "images": images}
+  contents.add(Content(typ: ctHistory, text: $inputShell))
   ajaxPost(apiUrl,
     headers = @[
       (cstring"mode", cstring"cors"),
@@ -154,149 +170,196 @@ proc croppedUrl(): string =
     result = result[0..<limit] & "..."
 
 proc createDom(): VNode =
-  result = buildHtml(tdiv):
+  result = buildHtml(tdiv(class = "main-area")):
+    table:
+      thead:
+        tr:
+          th()
+          th()
+      tbody:
+        for content in contents:
+          tr:
+            case content.typ
+            of ctInfo:
+              td()
+              td:
+                tdiv(class = "content ct-info"):
+                  text content.text
+            of ctHistory:
+              td(class = "command-marker"):
+                text "#"
+              td:
+                tdiv(class = "content ct-history"):
+                  text content.text
+            of ctStdout:
+              td()
+              td:
+                tdiv(class = "content ct-stdout"):
+                  text content.text
+            of ctStderr:
+              td()
+              td:
+                tdiv(class = "content ct-stderr"):
+                  text content.text
 
-    # ヘッダー
-    section(class = "hero is-dark"):
-      tdiv(class = "hero-body"):
-        tdiv(class = "container"):
-          tdiv(class = "content has-text-centered"):
-            h1(class = "title"): text "websh"
+        tr:
+          td(class = "command-marker"):
+            text "#"
+          td:
+            tdiv(class = "text-input-area"):
+              tdiv:
+                textarea(class = "text-input", setFocus = true,
+                    onkeydown = inputTextareaOnkeydown,
+                    onkeyup = inputTextareaOnkeyup,
+                    placeholder = "Enter your shellgei. ex: echo hello world", row = "1"):
+                  text inputShell
+        tr:
+          td()
+          td:
+            button(class = "button", onclick = sendShellButtonOnClick):
+              text "Run (Ctrl + Enter)"
 
-    # システムメッセージの表示
-    nav(class = "level"):
-      tdiv(class = "level-item has-text-centered"):
-        tdiv:
-          p(class = "heading"): text "system message"
-          p(class = "title"):
-            if isProgress:
-              text "Running ..."
-            elif outputStatus != statusOk:
-              text &"{outputSystemMessage} ({outputElapsedTime})"
-            else:
-              text &"none ({outputElapsedTime})"
+    # # ヘッダー
+    # section(class = "hero is-dark"):
+    #   tdiv(class = "hero-body"):
+    #     tdiv(class = "container"):
+    #       tdiv(class = "content has-text-centered"):
+    #         h1(class = "title"): text "websh"
 
-    # 入力、出力スペース
-    tdiv(class = "columns is-desktop"):
-      tdiv(class = "column"):
-        tdiv(class = "tile is-ancestor"):
-          tdiv(class = "tile is-parent is-vertical"):
-            article(class = "tile is-child notification"):
-              p(class = "title"): text "input"
-              p(class = "subtitle"):
-                tdiv:
-                  let count = countWord($inputShell)
-                  tdiv:
-                    text &"{$count} chars"
-                  let remain = 280 - count
-                  let remainPercent = int(count / 280 * 100)
-                  let color =
-                    if 100 <= remainPercent: "has-background-danger"
-                    elif 70 <= remainPercent: "has-background-warning"
-                    else: ""
-                  tdiv(class = color):
-                    text &"Remaining: {$remain} chars ({$remainPercent}%)."
-              tdiv(class = "content"):
-                tdiv:
-                  textarea(class = "textarea is-primary",
-                           placeholder = "ex: echo 'Hello shell'",
-                           rows = "15",
-                           setFocus = true,
-                           onkeydown = inputTextareaOnkeydown,
-                           onkeyup = inputTextareaOnkeyup,
-                           ):
-                    text inputShell
-                for ii in 0..<inputImages.len:
-                  tdiv(class="file has-name is-primary"):
-                    label(class="file-label"):
-                      input(class="file-input", accept="image/*", `type`="file", name="images", onchange=uploadFileButtonOnClicked(ii))
-                      span(class="file-cta"):
-                        span(class="file-label"):
-                          text "image file"
-                      span(class="file-name"):
-                        if inputImages[ii].name == cstring"":
-                          text "no file"
-                        else:
-                          text inputImages[ii].name
-                    if inputImages[ii].name != cstring"":
-                      button(class="button has-background-danger", onclick=deleteFileButtonOnClicked(ii)):
-                        text "Delete"
-                tdiv(class = "buttons"):
-                  button(class="button is-primary", onclick = sendShellButtonOnClick):
-                    text "Run (Ctrl + Enter)"
-                  a(href = &"""https://twitter.com/intent/tweet?hashtags={encodeUrl($hashTag, false)}&text={encodeUrl($inputShell, false)}&ref_src=twsrc%5Etfw""",
-                      # #204 Karaxと干渉しているため
-                      # class = "button twitter-share-button is-link",
-                      # `data-show-count` = "false"):
-                      class = "button is-link"):
-                    text "Tweet"
-                  tdiv(class = "select"):
-                    select:
-                      proc onchange(ev: Event, n: VNode) =
-                        hashTag = $n.value
-                      for elem in @[cstring"シェル芸", cstring"shellgei", cstring"ゆるシェル", cstring"危険シェル芸", ]:
-                        option(value = $elem):
-                          text $elem
-                tdiv:
-                  a(href = &"{baseUrl}?code={encodeUrl($inputShell, false)}"):
-                    text croppedUrl()
-            article(class = "tile is-child notification"):
-              p(class = "title"):
-                text "history"
-              tdiv(class = "content"):
-                tdiv(class = "buttons"):
-                  button(class="button is-danger", onclick = clearShellHistory):
-                    text "Clear history"
-                for hist in shellHistory:
-                  tdiv:
-                    textarea(class = "textarea is-success", rows = "2", readonly="readonly"):
-                      text hist
-      tdiv(class = "column"):
-        tdiv(class = "tile is-ancestor"):
-          tdiv(class = "tile is-parent is-vertical"):
-            article(class = "tile is-child notification"):
-              p(class = "title"): text "stdout"
-              p(class = "subtitle"):
-                tdiv:
-                  text &"{countWord($outputStdout)} chars, "
-                  text &"""{$($outputStdout).split("\n").len} lines"""
-                tdiv:
-                  textarea(class = "textarea is-success", rows = "8", readonly="readonly"):
-                    text outputStdout
-            article(class = "tile is-child notification"):
-              p(class = "title"): text "stderr"
-              p(class = "subtitle"):
-                tdiv:
-                  text &"{countWord($outputStderr)} chars, "
-                  text &"""{$($outputStderr).split("\n").len} lines"""
-                tdiv:
-                  textarea(class = "textarea is-warning", rows = "4", readonly="readonly"):
-                    text outputStderr
-            article(class = "tile is-child notification"):
-              p(class = "title"): text "images"
-              tdiv(class = "content"):
-                for img in outputImages:
-                  tdiv:
-                    # imgでbase64を表示するときに必要なメタ情報を追加
-                    case $img.format
-                    of "gif":
-                      img(src = "data:image/gif;base64," & img.image)
-                    else:
-                      img(src = "data:image/png;base64," & img.image)
-                  tdiv:
-                    text &"{img.filesize} byte"
+    # # システムメッセージの表示
+    # nav(class = "level"):
+    #   tdiv(class = "level-item has-text-centered"):
+    #     tdiv:
+    #       p(class = "heading"): text "system message"
+    #       p(class = "title"):
+    #         if isProgress:
+    #           text "Running ..."
+    #         elif outputStatus != statusOk:
+    #           text &"{outputSystemMessage} ({outputElapsedTime})"
+    #         else:
+    #           text &"none ({outputElapsedTime})"
 
-    footer(class = &"footer"):
-      tdiv(class = "container"):
-        tdiv(class = "content has-text-centered"):
-          text "© 2019, jiro4989 ("
-          a(href = "https://twitter.com/jiro_saburomaru"): text "@jiro_saburomaru"
-          text "), Apache License, "
-          a(href = "https://github.com/jiro4989/websh"): text "Repository"
-          text ", "
-          a(href = "https://status-websh.site24x7signals.com"): text "Public Status Page"
-        tdiv(class = "content has-text-centered"):
-          text "tag: " & tag & ", revision: " & revision
+    # # 入力、出力スペース
+    # tdiv(class = "columns is-desktop"):
+    #   tdiv(class = "column"):
+    #     tdiv(class = "tile is-ancestor"):
+    #       tdiv(class = "tile is-parent is-vertical"):
+    #         article(class = "tile is-child notification"):
+    #           p(class = "title"): text "input"
+    #           p(class = "subtitle"):
+    #             tdiv:
+    #               let count = countWord($inputShell)
+    #               tdiv:
+    #                 text &"{$count} chars"
+    #               let remain = 280 - count
+    #               let remainPercent = int(count / 280 * 100)
+    #               let color =
+    #                 if 100 <= remainPercent: "has-background-danger"
+    #                 elif 70 <= remainPercent: "has-background-warning"
+    #                 else: ""
+    #               tdiv(class = color):
+    #                 text &"Remaining: {$remain} chars ({$remainPercent}%)."
+    #           tdiv(class = "content"):
+    #             tdiv:
+    #               textarea(class = "textarea is-primary",
+    #                        placeholder = "ex: echo 'Hello shell'",
+    #                        rows = "15",
+    #                        setFocus = true,
+    #                        onkeydown = inputTextareaOnkeydown,
+    #                        onkeyup = inputTextareaOnkeyup,
+    #                        ):
+    #                 text inputShell
+    #             for ii in 0..<inputImages.len:
+    #               tdiv(class="file has-name is-primary"):
+    #                 label(class="file-label"):
+    #                   input(class="file-input", accept="image/*", `type`="file", name="images", onchange=uploadFileButtonOnClicked(ii))
+    #                   span(class="file-cta"):
+    #                     span(class="file-label"):
+    #                       text "image file"
+    #                   span(class="file-name"):
+    #                     if inputImages[ii].name == cstring"":
+    #                       text "no file"
+    #                     else:
+    #                       text inputImages[ii].name
+    #                 if inputImages[ii].name != cstring"":
+    #                   button(class="button has-background-danger", onclick=deleteFileButtonOnClicked(ii)):
+    #                     text "Delete"
+    #             tdiv(class = "buttons"):
+    #               button(class="button is-primary", onclick = sendShellButtonOnClick):
+    #                 text "Run (Ctrl + Enter)"
+    #               a(href = &"""https://twitter.com/intent/tweet?hashtags={encodeUrl($hashTag, false)}&text={encodeUrl($inputShell, false)}&ref_src=twsrc%5Etfw""",
+    #                   # #204 Karaxと干渉しているため
+    #                   # class = "button twitter-share-button is-link",
+    #                   # `data-show-count` = "false"):
+    #                   class = "button is-link"):
+    #                 text "Tweet"
+    #               tdiv(class = "select"):
+    #                 select:
+    #                   proc onchange(ev: Event, n: VNode) =
+    #                     hashTag = $n.value
+    #                   for elem in @[cstring"シェル芸", cstring"shellgei", cstring"ゆるシェル", cstring"危険シェル芸", ]:
+    #                     option(value = $elem):
+    #                       text $elem
+    #             tdiv:
+    #               a(href = &"{baseUrl}?code={encodeUrl($inputShell, false)}"):
+    #                 text croppedUrl()
+    #         article(class = "tile is-child notification"):
+    #           p(class = "title"):
+    #             text "history"
+    #           tdiv(class = "content"):
+    #             tdiv(class = "buttons"):
+    #               button(class="button is-danger", onclick = clearShellHistory):
+    #                 text "Clear history"
+    #             for hist in shellHistory:
+    #               tdiv:
+    #                 textarea(class = "textarea is-success", rows = "2", readonly="readonly"):
+    #                   text hist
+    #   tdiv(class = "column"):
+    #     tdiv(class = "tile is-ancestor"):
+    #       tdiv(class = "tile is-parent is-vertical"):
+    #         article(class = "tile is-child notification"):
+    #           p(class = "title"): text "stdout"
+    #           p(class = "subtitle"):
+    #             tdiv:
+    #               text &"{countWord($outputStdout)} chars, "
+    #               text &"""{$($outputStdout).split("\n").len} lines"""
+    #             tdiv:
+    #               textarea(class = "textarea is-success", rows = "8", readonly="readonly"):
+    #                 text outputStdout
+    #         article(class = "tile is-child notification"):
+    #           p(class = "title"): text "stderr"
+    #           p(class = "subtitle"):
+    #             tdiv:
+    #               text &"{countWord($outputStderr)} chars, "
+    #               text &"""{$($outputStderr).split("\n").len} lines"""
+    #             tdiv:
+    #               textarea(class = "textarea is-warning", rows = "4", readonly="readonly"):
+    #                 text outputStderr
+    #         article(class = "tile is-child notification"):
+    #           p(class = "title"): text "images"
+    #           tdiv(class = "content"):
+    #             for img in outputImages:
+    #               tdiv:
+    #                 # imgでbase64を表示するときに必要なメタ情報を追加
+    #                 case $img.format
+    #                 of "gif":
+    #                   img(src = "data:image/gif;base64," & img.image)
+    #                 else:
+    #                   img(src = "data:image/png;base64," & img.image)
+    #               tdiv:
+    #                 text &"{img.filesize} byte"
+
+    # footer(class = &"footer"):
+    #   tdiv(class = "container"):
+    #     tdiv(class = "content has-text-centered"):
+    #       text "© 2019, jiro4989 ("
+    #       a(href = "https://twitter.com/jiro_saburomaru"): text "@jiro_saburomaru"
+    #       text "), Apache License, "
+    #       a(href = "https://github.com/jiro4989/websh"): text "Repository"
+    #       text ", "
+    #       a(href = "https://status-websh.site24x7signals.com"): text "Public Status Page"
+    #     tdiv(class = "content has-text-centered"):
+    #       text "tag: " & tag & ", revision: " & revision
 
 when not defined modeTest:
   setRenderer createDom
